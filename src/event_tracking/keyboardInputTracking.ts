6020f6dd54cf6588b1e5acfc8d1d105660f97cc8
vuswltmd <sseung7367@gmail.com>
2024-09-02 23:00:13 +0900

    feat: keyboard input을 control input, data input으로 구분하여 수집하는 코드 추가
     #17

--- src/event_tracking/keyboardInputTracking.ts
+++ src/event_tracking/keyboardInputTracking.ts
@@ -19,7 +19,6 @@ export const onKeyboard = async (event: KeyboardEvent) => {
 
     // 첫 번째 입력인 경우 또는 마지막 입력이 임계값(1.0s)을 초과한 경우 'input start'로 설정
     if (now - lastKeyboardInputTime > KeyboardInputThreshold) {
-        console.log(`keyboard input: input start`);
 
         switch (currentKeyboardInput.code) {
             case "Numpad0":
@@ -34,13 +33,14 @@ export const onKeyboard = async (event: KeyboardEvent) => {
             case "Numpad9":
                 if (isNaN(parseInt(currentKeyboardInput.key)) == true) { // Numpad keys를 눌렀지만 숫자가 아닌 다른 부호를 클릭한 경우 키보드 입력이 종료되었다고 간주
                     lastKeyboardInputTime = now
-                    console.log(`Keyboard input: Not a number key, received ${currentKeyboardInput.key}`);
-                    keyboardLogEvent("keyboard input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
-                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`)
+                    // keyboardLogEvent("keyboard input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    // endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`)
+                    keyboardLogEvent("control input", event, false, Date.now(), " - ", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`, true)
                     return;
                 } else {
                     console.log("currentKeyboardEvent.key -> ", currentKeyboardInput.key)
-                    keyboardLogEvent("keyboard input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    keyboardLogEvent("data input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
                 }
                 break;
             case "Space":
@@ -58,19 +58,21 @@ export const onKeyboard = async (event: KeyboardEvent) => {
             case "NumpadEqual":
                 // Printable 키 또는 Non-printable 키 중 일부(Space, Shift, CapsLock 등)를 입력한 경우
                 console.log("currentKeyboardEvent.key -> ", currentKeyboardInput.key)
-                keyboardLogEvent("keyboard input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                keyboardLogEvent("data input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
                 break;
             default:
                 // Printable 키를 입력한 경우
                 if ((currentKeyboardInput.key.length == 1) && (currentKeyboardInput.key.match(/[a-z]/) || currentKeyboardInput.key.match(/[A-Z]/) || currentKeyboardInput.key.match(/[~`!@#$%^&*()-_+={[}\]|\\:;"'<,>.?/]/))) {
-                    console.log(`keyboard input: ${currentKeyboardInput.key}`);
-                    keyboardLogEvent("keyboard input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    console.log(`data input: ${currentKeyboardInput.key}`);
+                    keyboardLogEvent("data input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
                 } else {
                     //Start State에 입력한 키가 Non-printable Keys라면 시작과 동시에 종료
                     lastKeyboardInputTime = now
-                    console.log(`keyboard input: input end due to ${currentKeyboardInput.key}`);
-                    keyboardLogEvent("keyboard input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
-                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`)
+                    console.log(`control input: input end due to ${currentKeyboardInput.key}`);
+                    // keyboardLogEvent("keyboard input", event, false, Date.now(), "input start", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    // endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`)
+                    keyboardLogEvent("control input", event, false, Date.now(), " - ", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`, true)
                     return;
                 }
                 break;
@@ -92,11 +94,14 @@ export const onKeyboard = async (event: KeyboardEvent) => {
                 if (isNaN(parseInt(currentKeyboardInput.key)) == true) { // Numpad keys를 눌렀지만 숫자가 아닌 다른 부호를 클릭한 경우 키보드 입력이 종료되었다고 간주
                     lastKeyboardInputTime = now
                     console.log(`keyboard input: Not a number key, received ${currentKeyboardInput.key}`);
-                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`)
+                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`, false)
+
+                    keyboardLogEvent("control input", event, false, Date.now() + 2, " - ", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    endKeyboardInput(Date.now() + 3, `${currentKeyboardInput.key}`, true)
                     return;
                 } else {
                     console.log("currentKeyboardEvent.key -> ", currentKeyboardInput.key)
-                    keyboardLogEvent("keyboard input", event, false, Date.now(), "input ongoing", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    keyboardLogEvent("data input", event, false, Date.now(), "input ongoing", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
                 }
                 break;
             case "Space":
@@ -113,21 +118,24 @@ export const onKeyboard = async (event: KeyboardEvent) => {
             case "NumpadEnter":
             case "NumpadEqual":
                 // Printable 키 또는 Non-printable 키 중 일부(Space, Shift, CapsLock 등)를 입력한 경우
-                console.log(`keyboard input: input ongoing`);
+                console.log(`data input: input ongoing`);
                 console.log("currentKeyboardEvent.key -> ", currentKeyboardInput.key)
-                keyboardLogEvent("keyboard input", event, false, Date.now(), "input ongoing", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                keyboardLogEvent("data input", event, false, Date.now(), "input ongoing", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
                 break;
             default:
                 // Printable 키를 입력한 경우
                 if ((currentKeyboardInput.key.length == 1) && (currentKeyboardInput.key.match(/[a-z]/) || currentKeyboardInput.key.match(/[A-Z]/) || currentKeyboardInput.key.match(/[~`!@#$%^&*()-_+={[}\]|\\:;"'<,>.?/]/))) {
-                    console.log(`keyboard input: input ongoing`);
-                    console.log(`keyboard input: ${currentKeyboardInput.key}`);
-                    keyboardLogEvent("keyboard input", event, false, Date.now(), "input ongoing", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    console.log(`data input: input ongoing`);
+                    console.log(`data input: ${currentKeyboardInput.key}`);
+                    keyboardLogEvent("data input", event, false, Date.now(), "input ongoing", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
                 } else {
                     //Start State에 입력한 키가 Non-printable Keys라면 시작과 동시에 종료
                     lastKeyboardInputTime = now
-                    console.log(`keyboard input: input end due to ${currentKeyboardInput.key}`);
-                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`)
+                    console.log(`data input: input end due to ${currentKeyboardInput.key}`);
+                    endKeyboardInput(Date.now() + 1, `${currentKeyboardInput.key}`, false)
+
+                    keyboardLogEvent("control input", event, false, Date.now() + 2, " - ", currentKeyboardInput.type, currentKeyboardInput.key, currentKeyboardInput.code);
+                    endKeyboardInput(Date.now() + 3, `${currentKeyboardInput.key}`, true)
                     return;
                 }
                 break;
@@ -141,7 +149,7 @@ export const onKeyboard = async (event: KeyboardEvent) => {
         console.log(`keyboard input: input end`);
         keyboardInputTimeout = undefined;
         if (currentKeyboardInput !== null) {
-            keyboardLogEvent("keyboard input", currentKeyboardInput, true, Date.now(),
+            keyboardLogEvent("data input", currentKeyboardInput, true, Date.now(),
                 "input end", //keyboardInputState
                 undefined, // keyboardInputType
                 undefined, // keyboardInputPressedKey
@@ -160,32 +168,48 @@ export const onKeyboard = async (event: KeyboardEvent) => {
  * @param {number} time - 키보드 입력 종료 시간
  * @param {string} [isEscapeKey] - 종료를 트리거한 키 (옵션)
  */
-export const endKeyboardInput = (time: any, isEscapeKey?: string) => {
+export const endKeyboardInput = (time: any, isEscapeKey?: string, isControlKey?: boolean) => {
     if (lastKeyboardInputTime !== 0 || keyboardInputTimeout) {
         clearTimeout(keyboardInputTimeout); // 기존 타이머 취소
         keyboardInputTimeout = undefined; // 타이머 상태 초기화
         lastKeyboardInputTime = 0; // 키보드 입력 타이머 리셋
 
-        if (currentKeyboardInput !== null) {
-            if (isEscapeKey) {
-                //키보드 입력을 종료시키는 Non-printable keys가 입력된 경우
-                console.log(`input end due to pressing ${isEscapeKey}`);
-                keyboardLogEvent("keyboard input", currentKeyboardInput, true, time,
-                    `input end due to pressing ${isEscapeKey} key`, //keyboardInputState
-                    undefined, // keyboardInputType
-                    undefined, // keyboardInputPressedKey
-                    undefined // keyboardInputKeyCode
-                )
-            }
-            else {
-                //다른 이벤트 (예: 마우스 클릭, 스크롤)가 간섭한 경우
-                console.log(`keyboard input: input end due to other event`);
-                keyboardLogEvent("keyboard input", currentKeyboardInput, true, time,
-                    "input end due to other event", //keyboardInputState
-                    undefined, // keyboardInputType
-                    undefined, // keyboardInputPressedKey
-                    undefined // keyboardInputKeyCode 
-                );
+        if (isControlKey) {
+
+        } else {
+            if (currentKeyboardInput !== null) {
+                if (isEscapeKey) {
+                    //키보드 입력을 종료시키는 Non-printable keys가 입력된 경우
+                    console.log(`input end due to pressing ${isEscapeKey}`);
+                    keyboardLogEvent("data input", currentKeyboardInput, true, time,
+                        "input end", //keyboardInputState
+                        undefined, // keyboardInputType
+                        undefined, // keyboardInputPressedKey
+                        undefined // keyboardInputKeyCode
+                    )
+                    // keyboardLogEvent("data input", currentKeyboardInput, true, time,
+                    //     `input end due to pressing ${isEscapeKey} key`, //keyboardInputState
+                    //     undefined, // keyboardInputType
+                    //     undefined, // keyboardInputPressedKey
+                    //     undefined // keyboardInputKeyCode
+                    // )
+                }
+                else {
+                    //다른 이벤트 (예: 마우스 클릭, 스크롤)가 간섭한 경우
+                    console.log(`data input: input end due to other event`);
+                    keyboardLogEvent("data input", currentKeyboardInput, true, time,
+                        "input end", //keyboardInputState
+                        undefined, // keyboardInputType
+                        undefined, // keyboardInputPressedKey
+                        undefined // keyboardInputKeyCode 
+                    );
+                    // keyboardLogEvent("data input", currentKeyboardInput, true, time,
+                    //     "input end due to other event", //keyboardInputState
+                    //     undefined, // keyboardInputType
+                    //     undefined, // keyboardInputPressedKey
+                    //     undefined // keyboardInputKeyCode 
+                    // );
+                }
             }
         }
     }
